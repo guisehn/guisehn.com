44e1f7c3631050982ed7ca13e3f610f41bbcd827
Guilherme Sehn <guilherme.sehn@therealreal.com>
2024-05-26 15:42:16 -0300

    fix rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,15 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: "Gui Sehn",
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  return rss({
+    title: "guisehn.com",
+    description: "",
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/${post.slug}/`,
+    })),
+  });
 }
